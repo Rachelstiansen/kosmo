66ecca480d2c03ce41dd4158dc2b5d7a1cecc818
rachelstiansen <rachelstiansen@gmail.com>
2024-04-24 12:55:59 +0200

    first draft

--- project 2/BBN.py
+++ project 2/BBN.py
@@ -1,3 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy import units as au, constants as ac
+from scipy.integrate import solve_ivp
+
+# Own imports
+
--- project 2/background.py
+++ project 2/background.py
@@ -1,3 +1,4 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy import units as au, constants as ac
+
