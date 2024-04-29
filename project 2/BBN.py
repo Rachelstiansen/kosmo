overflow mistake...

project 2/BBN.py
@@ -24,7 +24,8 @@ class BBN:
         self.RR = ReactionRates() # Initiate the set of reaction rates equations
         
         # Initiate the background equations , e.g. equations for the Hubble parameter and rho_b :
-        self.background = Background(**background_kwargs )
+        self.background = Background(**background_kwargs)
+        self.cgs = cgs()
 
     def get_ODE(self, lnT: float, Y: np.ndarray) -> np.ndarray:
         """
@@ -71,14 +72,14 @@ class BBN:
 
     def get_np_equil(self, T_i):
         """
-        Not sure if this is the way to call the constants
+        ...
         """
-        Y_n = (1 + np.exp((self.background.cgs.m_n - self.background.cgs.m_p) * self.background.cgs.c**2 / (self.background.cgs.k_B * T_i)))**(-1)
+        Y_n = (1 + np.exp((self.cgs.m_n - self.cgs.m_p) * self.cgs.c**2 / (self.cgs.k_B * T_i)))**(-1)
         Y_p = 1 - Y_n
 
         return Y_n, Y_p
 
-    def get_IC (self, T_init):
+    def get_IC(self, T_init):
         """
         Defines the initial condition array used in solve_BBN.
         The only nonzero values are for neutrons and protons, but the shape of self.Y_init is
@@ -90,7 +91,7 @@ class BBN:
         Y_init = np.zeros(self.NR_species) # Initializes all species to zero
         Y_n_init, Y_p_init = self.get_np_equil(T_init) # Solves eq 16-17
         Y_init[0] = Y_n_init
-        Y_n_init[1] = Y_p_init
+        Y_init[1] = Y_p_init
         
         return Y_init
     
@@ -106,15 +107,6 @@ class BBN:
         lnT = np.linspace(sol.t[0], sol.t[-1], self.NR_points)
         self.Y_i = sol.sol(lnT)
         self.T = np.exp(lnT)
-
-if __name__ == " __main__ ":
-    # Example use:
-    # Initiate the system including 3 species ( neutrons , protons and deuterium ):
-    bbn = BBN(NR_interacting_species = 3)
-    bbn.solve_BBN(T_end = 0.1e9) # solve the system until end temperature 0.1*10^9 K
+        
+        return self.T, self.Y_i
     
-    # Plot the mass fraction for each species :
-    fig, ax = plt.subplots ()
-    for i, y in enumerate (bbn.Y_i ):
-        ax.loglog(bbn.T, bbn.mass_number[i] * y, label = bbn.species_labels[i])
-

project 2/background.py
@@ -9,25 +9,32 @@ class Background:
         self.h = 0.7
 
         self.Omega_b0 = 0.05
-        self.H_0 = (100 * self.h * au.km / (au.second * au.Mpc))
-        self.Omega_r0 = 8 * np.pi**3 * ac.G * (ac.k_B * self.T_0)**4 * (1 + self.N_eff * (7/8) * (4/11)**(4/3)) / (45 * self.H_0**2 * ac.hbar**3 * ac.c**5)
+        self.H_0 = (100 * self.h * au.km / (au.second * au.Mpc)).cgs.value
+        self.Omega_r0 = (8 * np.pi**3 * ac.G * (ac.k_B * self.T_0)**4 * (1 + self.N_eff * (7/8) * (4/11)**(4/3)) / (45 * self.H_0**2 * ac.hbar**3 * ac.c**5)).cgs.value
 
-    def cgs(self):
-        k_B = ac.k_B.cgs.value
-        G = ac.G.cgs.value
-        h_bar = ac.hbar.cgs.value
-        c = ac.c.cgs.value
-        m_n = ac.m_n.value
-        m_p = ac.m_p.value
+        self.rho_co = 3 * self.H_0**2 / (8 * np.pi * ac.G.cgs.value)
+        self.rho_b0 = self.Omega_b0 * self.rho_co
 
-        self.H_0 = self.H_0.cgs.value
-        self.Omega_r0 = self.Omega_r0.cgs.value
-
-    def T_nu(T):
+    def T_nu(self, T):
         return (4/11)**(1/3) * T
+    
+    def a(self, T):
+        return self.T_0 / T
+    
+    def get_Hubble(self, T):
+        return self.H_0 * np.sqrt(self.Omega_r0) / self.a(T)**2
 
-    def get_Hubble(T):
-        return ...
+    def get_rho_b(self, T):
+        return self.Omega_b0 * self.rho_co / self.a(T)**3
 
-    def get_rho_b(T):
-        ...
+class cgs:
+    def __init__(self):    
+        self.k_B = ac.k_B.cgs.value
+        self.G = ac.G.cgs.value
+        self.h_bar = ac.hbar.cgs.value
+        self.c = ac.c.cgs.value
+        self.m_n = ac.m_n.cgs.value
+        self.m_p = ac.m_p.cgs.value
+        
+        # self.H_0 = self.H_0.cgs.value
+        # self.Omega_r0 = self.Omega_r0.cgs.value

project 2/reaction_rates.py
@@ -8,28 +8,25 @@ class ReactionRates:
         pass
 
 
-    def get_rate_weak(T):
+    def get_rate_weak(self, T9):
         """
         T = T9
         """
-        T_nu = (4/11)**(1/3) * T
+        T_nu = (4/11)**(1/3) * T9
         tau = 1700 # [s] free neutron decay time
         q = 2.53 # (m_n - m_p) / m_e
 
-        Z = 5.93 / T
+        Z = 5.93 / T9
         Z_nu = 5.93 / T_nu
 
         def I_n_p(x):
-            return (x + q)**2 * (x**2 - 1)**(1/2) * x / ((1 + np.exp(x * Z)) * (1 + np.exp(-(x + q) * Z_nu))) \
-                + (x - q)**2 * (x**2 - 1)**(1/2) * x / ((1 + np.exp(-x * Z)) * (1 + np.exp((x - q) * Z_nu)))
+            return (x + q)**2 * (x**2 - 1)**(1/2) * x / ((1 + np.exp(x * Z)) * (1 + np.exp(-(x + q) * Z_nu))) + (x - q)**2 * (x**2 - 1)**(1/2) * x / ((1 + np.exp(-x * Z)) * (1 + np.exp((x - q) * Z_nu)))
 
         def I_p_n(x):
-            return (x - q)**2 * (x**2 - 1)**(1/2) * x / ((1 + np.exp(x * Z)) * (1 + np.exp(-(x - q) * Z_nu))) \
-                + (x + q)**2 * (x**2 - 1)**(1/2) * x / ((1 + np.exp(-x * Z)) * (1 + np.exp((x + q) * Z_nu)))
+            return (x - q)**2 * (x**2 - 1)**(1/2) * x / ((1 + np.exp(x * Z)) * (1 + np.exp(-(x - q) * Z_nu))) + (x + q)**2 * (x**2 - 1)**(1/2) * x / ((1 + np.exp(-x * Z)) * (1 + np.exp((x + q) * Z_nu)))
 
-
-        lambda_n = quad(I_n_p, 1, np.inf) / tau
-        lambda_p = quad(I_p_n, 1, np.inf) / tau
+        lambda_n = quad(I_n_p, 1, np.inf)[0] / tau
+        lambda_p = quad(I_p_n, 1, np.inf)[0] / tau
         
         return lambda_n, lambda_p
 