965a6fdb527cc2c5996d573a9de5532bea7253a6
rachelstiansen <rachelstiansen@gmail.com>
2024-05-04 14:02:54 +0200

    changed question d

--- project 2/question_d.py
+++ project 2/question_d.py
@@ -8,9 +8,9 @@ h = 0.7
 def t(T):
     H_0 = (100 * h * au.km / (au.second * au.Mpc))
     Omega_r0 = 8 * np.pi**3 * ac.G * (ac.k_B * T_0)**4 * (1 + (7/8) * N_eff * (4/11)**(4/3)) / (45 * ac.hbar**3 * ac.c**5 * H_0**2)
-    t = (T_0 / T)**2 / (2 * H_0 * np.sqrt(Omega_r0))
-    print(t.cgs)
+    t = (T_0 / T)**2 / (2 * H_0 * np.sqrt(Omega_r0)).cgs.value
+    return t
 
-t(10e10)
-
-print()
+print(f"t(T = 10^10 K) = {t(1e10):.2f} s")
+print(f"t(T = 10^9 K) = {t(1e9):.2f} s")
+print(f"t(T = 10^8 K) = {t(1e8):.2f} s")
